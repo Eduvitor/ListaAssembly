91f8aa294efc426c1995f9cfba6bc48173bec39c
Eduvitor <evitoryt1@gmail.com>
2024-06-08 20:18:33 -0300

    Adicionado contador de elementos e insercoes na lista, configurado mensagens de retorno

--- Final.s
+++ Final.s
@@ -18,7 +18,7 @@ string4:        .string "Digite: "
 string5:        .string "\nInforme o numero a ser adicionado: "
 lista_vazia:    .string "A lista está vazia!\n"
 succesInser:    .string "\nA inserção foi um sucesso!\n"
-failInser:      .string "\nOcorreu um problema na inserção!\n"
+#failInser:      .string "\nOcorreu um problema na inserção!\n"
 numInserir:     .string "\nNúmero a ser inserido: "
 stringStatic:   .string "\n###---Exibindo as estatisticas da lista---###"
 stringStatic1:  .string "\nMaior valor da lista: "
@@ -26,6 +26,7 @@ stringStatic2:  .string "\nMenor valor da lista: "
 stringStatic3:  .string "\nNumero de elementos na lista: "
 stringStatic4:  .string "\nQuantidade de insercoes na lista: "
 stringStatic5:  .string "\nQuantidade de remocoes realizados: "
+mensagemSair:   .string "\nObrigado por usar a lista encadeada, espero nos vermos novamente!"
 head:           .word   0   #Definindo o endereço de inicio da lista e definindo como null
                 .text
 main:   
@@ -34,6 +35,8 @@ main:
                 ecall
                 la a0, string2 #
                 ecall
+                li s4, 0 #Inicializa o registrador que vai guardar a quantidade de insercoes realizadas
+                li s5, 0 #Registrador que vai guardar a quantidade de remocoes
 print_menu:
                 li a7, 4
                 la a0, string3
@@ -42,11 +45,7 @@ print_menu:
                 ecall
                 li a7, 5
                 ecall
-                li s2, 0 #Inicializa o resgistrador que vai guardar o maior valor
-                li s3, 0 #Registrador que vai guardar o menor valor
-                li s4, 0 #Inicializa o registrador que vai guardar a quantidade de insercoes realizadas
-                li s5, 0 #Registrador que vai guardar a quantidade de remocoes
-                li s6, 0 #Registrador para guardar 
+                #li s2, 0 #Inicializa o resgistrador que vai guardar o maior valor
                 add t0, a0, zero # t0 = a0 + zero
                 #inicia as comparações para saber qual função chamar 
                 li t1, 1
@@ -119,8 +118,7 @@ call_statics:
                 j print_menu
 
 call_end:
-                li a7, 93
-                ecall
+                jal exit_list
 
 #####################---Implementação Das Funções---##########################
 
@@ -134,6 +132,7 @@ insert_int:                             #função para inserir um numero inteiro
                 ecall
                 li t6, -1               #Carrega o valor de erro de alocacao -1
                 beq a0, t6, error_malloc   #Testa se o endereco retornado n
+                addi s4, s4, 1          #Atualiza o contador de insercoes realizadas, depois de comparar se a alocacao foi bem sucedida
                 sw a1, 0(a0)            #adicionando o primeiro valor para a lista
                 sw zero, 4(a0)          #define o proximo ponteiro como null
                 beqz t0, insert_ifnull  #se a lista tiver totalmente vazia apenas adicionamos no inicio
@@ -167,7 +166,7 @@ insert_ifnull:
                 li a0, 1		 #Retorno
                 ret
 insert_notnull:
-                sw a0, 4(t0) # 
+                sw a0, 4(t0)             #Insere no fim da lista
                 li a0, 1		 #Retorno
                 ret
 
@@ -209,18 +208,21 @@ empity_list:    #informa se a lista tá vazia
 #Fim dos labels para a função de printar uma lista
 print_statics: #Estatisticas da lista
                 #capturar o menor valor da lista
+                li s6, 0 #Registrador para guardar o numero de elementos na lista
                 lw t0, 0(a0) #Salva o endereco do primeiro elemento
-                lw s3, 0(t0) #Salva o menor valor da lista como esta ordenada e sempre o primeiro.
                 li a7, 4
-                la a0, stringStatic2
+                la a0, stringStatic2    
                 ecall
                 li a7, 1
-                mv a0, s3
-                ecall           
+                #mv a0, s3
+                lw a0, 0(t0)
+                ecall        #Printa o menor da lista
                 #vai atras do maior valor
 percorrer_fim:
                 lw t0, 4(t0) #Carrega o proximo valor presente na lista
                 #bnez t0, percorrer_fim
+                #S6 guarda o numero de elementos
+                addi s6, s6, 1 # s6 = s6 + 1 #Conta quantos elementos a lista possui
                 beq t0, zero, printa_maior
                 mv t1, t0       #Pega o item da lista anterior, util quando chegar no ultimo
                 j percorrer_fim
@@ -232,4 +234,23 @@ printa_maior:
                 lw a0, 0(t1)    #Carrega em a0 o valor do ultimo node
                 #Percorre a lista e encontra o maior valor que esta no fim da lista
                 ecall
+                li a7, 4
+                la a0, stringStatic3 #printa o numero de elementos na lista
+                ecall
+                li a7, 1 
+                mv a0, s6
+                ecall
+                la a0, stringStatic4 #printa o numero de insercoes realizadas na lista 
+                li a7, 4
+                ecall
+                mv a0, s4
+                li a7, 1
+                ecall
                 ret
+
+exit_list:
+                li a7, 4
+                la a0, mensagemSair
+                ecall
+                li a7, 93
+                ecall
